afcf12e4b1e06fd94796845da1d0bcae0c4b0ab8
Manshad Changampalli <manshadchangampalli010@gmail.com>
2026-05-18 22:24:49 +0400

    chore: update CORS origin to production frontend URL

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://one-mark-frontend.vercel.app',
     credentials: true,
   });
   app.use(cookieParser());
